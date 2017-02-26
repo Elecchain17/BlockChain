0a5bc0eaebca716d5f3291c40328d2cd967c13af
nodstuff <tom.meaney@mycit.ie>
2017-02-26 10:58:55 +0000

    remove sleep dependency

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var bodyparser = require('')
 var app = express();
 var initialize = require('./helloblockchain.js');
 
 // initialize.init();
+app.post('/vote', function(req, res){
+    var candidate = req.body.candidate;
+    var vote = req.body.vote
+}
